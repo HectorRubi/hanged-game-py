2ac8685f334d0e915a609eda5ac03b7b737ac120
Hector Rubi <hector.rubi@guros.com>
2021-09-26 20:35:54 -0500

    Get the secret word form the data file

--- index.py
+++ index.py
@@ -1,6 +1,21 @@
+import random
+
+def get_words_list():
+    words_list = []
+    with open('data.txt', 'r', encoding='utf=8') as f:
+        for line in f:
+            words_list.append(line)
+    return words_list
+
+
+def get_secret_word():
+    return random.choice(get_words_list())
+
+
 def run():
-    pass
+    secret_word = get_secret_word()
+    print(secret_word)
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     run()
